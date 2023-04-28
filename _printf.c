520eb6260527856649c89ebbdb1bf077fe2102b7
DennisMukirai <dennismukirai17@gmail.com>
2023-04-28 12:44:24 -0700

    commit

--- _printf.c
+++ _printf.c
@@ -8,7 +8,7 @@ void print_buffer(char buffer[], int *buff_ind);
  * return: printed chars
  */
 
-int _printf(const char *format)
+int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
